216745e83f67b2643d8b33e638cd1fdaa43da7bc
sukasukangoding <sukasukangoding@gmail.com>
2020-05-03 02:51:49 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './container/pages/app';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./container/pages/app";
+import * as serviceWorker from "./serviceWorker";
 import firebase from "./config/firebase";
 
 console.log(firebase);
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
