9c26ba3a27f58362e41bc98190c222bd4b246562
sukasukangoding <sukasukangoding@gmail.com>
2020-05-03 02:26:54 +0700

    Updates

---
+++ precache-manifest.9fe8f5a8a1bcc4ef4ce022c14ad84680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f50cf42aa52c839fab1eb09d24332",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7473f915ba51e02edfa8",
+    "url": "/react-todo-app/static/css/main.5e041f9e.chunk.css"
+  },
+  {
+    "revision": "c5609044e276c380d79b",
+    "url": "/react-todo-app/static/js/2.5017cd1d.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/react-todo-app/static/js/2.5017cd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7473f915ba51e02edfa8",
+    "url": "/react-todo-app/static/js/main.ea954e0f.chunk.js"
+  },
+  {
+    "revision": "c52d99abd5d9c389adf4",
+    "url": "/react-todo-app/static/js/runtime-main.f762f98d.js"
+  }
+]);
