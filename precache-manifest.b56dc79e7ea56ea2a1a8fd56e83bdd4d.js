1ab94398d64b9a02e78a7628d5a10d099513e3ff
sukasukangoding <sukasukangoding@gmail.com>
2020-05-03 02:56:01 +0700

    Updates

--- precache-manifest.b56dc79e7ea56ea2a1a8fd56e83bdd4d.js
+++ precache-manifest.b56dc79e7ea56ea2a1a8fd56e83bdd4d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "360f50cf42aa52c839fab1eb09d24332",
+    "revision": "96409d3ab865621af93a55e04ac657cc",
     "url": "/react-todo-app/index.html"
   },
   {
-    "revision": "7473f915ba51e02edfa8",
+    "revision": "54e1dba3858fb4b37c4e",
     "url": "/react-todo-app/static/css/main.5e041f9e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-todo-app/static/js/2.5017cd1d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7473f915ba51e02edfa8",
-    "url": "/react-todo-app/static/js/main.ea954e0f.chunk.js"
+    "revision": "54e1dba3858fb4b37c4e",
+    "url": "/react-todo-app/static/js/main.00f8b902.chunk.js"
   },
   {
     "revision": "c52d99abd5d9c389adf4",
